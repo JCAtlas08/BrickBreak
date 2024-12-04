3a0deb07364c0b8a7896854950070f681df3554d
JCAtlas08 <campjosh21@gmail.com>
2024-12-04 05:14:47 +0000

    commit -m "submission 12/4"

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,65 @@
 import java.awt.*;
 
 public class Ball {
-	//your code here!
+	//Instance Variables
+	private int xPos;
+	private int yPos;
+	private int xVelocity;
+	private int yVelocity;
+	private int size;
 	
-	//don't forget you need instance variables:
+	//Constructor
+	public Ball(int xPos, int yPos, int size) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.size = size;
+	}
 	
-	
-	//constructor(s):
-	
-	//methods:
+	//Precondition: g is not null
+	//Postcondition: the ball is drawn with the color white with size
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+        g.fillOval(xPos, yPos, size, size);
+	}
+
+	//Getters
+	public int getYpos() {
+		return yPos;
+	}
+	public int getXpos() {
+		return xPos;
+	}
+	public int getSize() {
+		return size;
+	}
+
+	//Setters
+	public void setY(int y) {
+		yPos = y;
+	}
+	public void setX(int x) {
+		xPos = x;
+	}
+	public void setYVelocity(int yVel) {
+		yVelocity = yVel;
+	}
+	public void setXVelocity(int xVel) {
+		xVelocity = xVel;
+	}
+
+	//Reversals
+	public void reverseY() {
+		yVelocity *= -1;
+	}
+	public void reverseX() {
+		xVelocity *= -1;
+	}
+
+	//Movement
+	public void move() {
+		xPos += xVelocity;
+		yPos += yVelocity;
+	}
+
 
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,63 @@
-import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
+	//Instance Variables
+	private int x;
+	private int y;
+	private int velocity;
+	private int width;
+	private int height;
+	
+	//Constructor
+	public Paddle(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
 
-	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
-	
+	//Precondition: g is not null
+	//Postcondition: the paddle is drawn white at x and y with the dimentions width*height
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(x, y, width, height);
+	}
+
+	//Getters
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public int getVelocity() {
+		return velocity;
+	}
+
+	//Setters
+	public void setX(int newX) {
+		x = newX;
+	}
+	public void setVelocity(int newVel) {
+		velocity = newVel;
+	}
+
+	//Changers
+	public void addVelocity(int a) {
+		velocity += a;
+	}
+
+	//Movement
+	public void move() {
+		x += velocity;
+		velocity *= 0.9;
+	}
+
 	
 }
